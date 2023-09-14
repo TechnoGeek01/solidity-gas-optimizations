a5885ade096685ad132c39ba377672b7d60c77d0
TechnoGeek01 <rsudharsangames@gmail.com>
2023-09-14 14:48:51 +0530

    updated

--- optimization-1.sol
+++ optimization-1.sol
@@ -1,10 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-// Runtime and deployment time gas cost
-// Ternary operation is cheaper than if-else statement. But in some latest solidity versions this is optimized by default.
+// Runtime and deployment time gas cost optimization
+// ternary operation is cheaper than if-else statement. But in some latest solidity versions this is optimized by default.
 // if you are using version 0.8.13 or more solidity compiler, the compiler does this optimization automatically for us.
-// but deployment cost is less in case of ternary operator irrespective of the compiler version for now.
+// but deployment cost is less in case of ternary operator irrespective of the compiler version for now. In future versions it might be optimized.
 
 contract test1 {
     uint i = 1;
